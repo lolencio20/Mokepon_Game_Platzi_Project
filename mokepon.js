81669ed7da7184d7fec76ebdb353f857011f8814
Emerson Gonzalez <emersonmm20@gmail.com>
2023-01-10 17:55:54 -0500

    se ha limpiado el codigo javascript -- version funcional 0.2.1

--- mokepon.js
+++ mokepon.js
@@ -1,36 +1,39 @@
-//seccion elejir mascota jugador
+//declaracion de variables: --------------------------------
 
-//errores actuales: no sabe cuando las vidas llegan a "0" 
-
-
-
-
-
-
-
-
- let displayMascotas=document.getElementById("seleccionar-mascota")
+//funcion seleccionar mascota enemigo
+let mascotaEnemigo=aleatorioEntre(1,5)
+//seleccionar mascota
+let botonMascotaJugador=document.getElementById("boton-seleccionar")
+//display's
+let displayMascotas=document.getElementById("seleccionar-mascota")
  ,displayAtaques=document.getElementById("seleccionar-ataque"),
  displayReset=document.getElementById('reiniciar'),
  displayNotificacion=document.querySelector('.notificacion');
-
+//combate:
+let ataqueJugador, ataquePC;
+let ataqueAgua=document.getElementById("boton-agua")
+let ataqueFuego=document.getElementById("boton-fuego")
+let ataqueTierra=document.getElementById("boton-tierra")
+let notificacion= document.querySelector(".notificacion")
+//funcion crear mensaje
+let ataquesJugador=document.querySelector(".ataques-jugador")
+let ataquesPc=document.querySelector(".ataques-pc")
+let notJugador=document.createElement("p")
+let notPc=document.createElement("p")
+//boton reinicio
+let botonReinicio=document.getElementById("boton-reiniciar")
+//--------------------------------declaracion de variables//
 displayAtaques.style.display= "none"
 displayReset.style.display="none"
 displayNotificacion.style.display="none"
 
-	let notificacion= document.querySelector(".notificacion");
-
-
 // ------------------------   SELECCION DE MASCOTAS           -----------------
-
-
 // Esta funcion nos permite mostrar la zona del combate y ocultar la zona de seleccionar personaje
 function mostrarCombate() {
 	displayMascotas.style.display = "none"
 	displayAtaques.style.display= "flex"
 	displayReset.style.display="block"
-	displayNotificacion.style.display="block"
-}
+	displayNotificacion.style.display="block"}
 
 
 function seleccionarMascotaJugador() {
@@ -58,11 +61,11 @@ function seleccionarMascotaJugador() {
 	else{alert("Por favor selecciona a tu mascota"); return 0}	
 
 
-	let mascotaEnemigo = seleccionarMascotaEnemigo()	
-}
+	let mascotaEnemigo = seleccionarMascotaEnemigo()	}
+botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 
 function seleccionarMascotaEnemigo(){
-	let mascotaEnemigo=aleatorioEntre(1,5)
+
 	if (mascotaEnemigo==1) {
 		mascotaEnemigo="Hipodoge"
 		document.getElementById("mascotaEnemigo").innerHTML= "Hipodoge"
@@ -82,35 +85,14 @@ function seleccionarMascotaEnemigo(){
 	else if (mascotaEnemigo==5) {
 		mascotaEnemigo="Tucapalma"
 		document.getElementById("mascotaEnemigo").innerHTML= "Tucapalma"
-	}
-	return mascotaEnemigo
-}
+	}}
 function aleatorioEntre(min,max){
-	return Math.floor(Math.random()*max +min)
-}
-
-
-let botonMascotaJugador=document.getElementById("boton-seleccionar")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
-
-
+	return Math.floor(Math.random()*max +min)}
 // ------------------------   COMBATE           -----------------
-
-
-
-let ataqueJugador, ataquePC;
-
-let vidasEnemigo=3
-let vidasJugador=3
-
-
-
 let desabiltarAtaques=()=>{
 	document.getElementById("boton-agua"). disabled= true
 	document.getElementById("boton-fuego"). disabled= true
-	document.getElementById("boton-tierra"). disabled= true
-}
-
+	document.getElementById("boton-tierra"). disabled= true}
 function revisarVidas(){
 	if (document.getElementById("vidasJugador").innerHTML==0){
 		notificacion.innerHTML="Que mal, has perdido"
@@ -125,31 +107,17 @@ function revisarVidas(){
 		desabiltarAtaques()
 
 
-		 }
-}
-
-let ataqueAgua=document.getElementById("boton-agua")
-
-	ataqueAgua.addEventListener("click",()=>{
-		ataqueJugador="AGUA"
-		ataqueEnemigo()
+		 }}
+ataqueAgua.addEventListener("click",()=>{
+	ataqueJugador="AGUA"
+	ataqueEnemigo()
 	})
-
-let ataqueFuego=document.getElementById("boton-fuego")
-	ataqueFuego.addEventListener("click",()=>{
-		ataqueJugador="FUEGO"
-		ataqueEnemigo()
-	})
-
-let ataqueTierra=document.getElementById("boton-tierra")
-
-	ataqueTierra.addEventListener("click",()=>{
-
-		ataqueJugador="TIERRA"
-		ataqueEnemigo()
-
-
-
+ataqueFuego.addEventListener("click",()=>{
+	ataqueJugador="FUEGO"
+	ataqueEnemigo()})
+ataqueTierra.addEventListener("click",()=>{
+	ataqueJugador="TIERRA"
+	ataqueEnemigo()
 	})
 let ataqueEnemigo=()=>{
 
@@ -163,90 +131,25 @@ let ataqueEnemigo=()=>{
 		ataquePC="TIERRA"
 	}
 	crearMensaje()
-	revisarVidas()
-
-}
-
+	revisarVidas()}
+let crearNotificacion= (e)=>{
+	if (e=="GANAS") {
+	document.getElementById("vidasEnemigo").innerHTML-= 1
+	notificacion.innerHTML=e}
+	else if(e=="PIERDES"){
+		document.getElementById("vidasJugador").innerHTML-=1
+		notificacion.innerHTML=e}
+	else {notificacion.innerHTML=e}}
 let crearMensaje=()=>{
-
-
-	let ataquesJugador=document.querySelector(".ataques-jugador")
-	let ataquesPc=document.querySelector(".ataques-pc")
-
-	let notJugador=document.createElement("p")
-	let notPc=document.createElement("p")
-
+	notJugador=document.createElement("p")
+	notPc=document.createElement("p")
 	notJugador.innerHTML=ataqueJugador
 	notPc.innerHTML=ataquePC
-
-
-
 	ataquesJugador.appendChild(notJugador)
 	ataquesPc.appendChild(notPc)
-
-
-
-//la funcion de crear notificacion va a recibir un parametro tipo string como "GANAS", "PIERDES" o "EMPATE"
-//lo que hay que hacer es que haga diferentes procedimientos para cada uno de los parametros que reciba
-	let crearNotificacion= (e)=>{
-
-		if (e=="GANAS") {
-		document.getElementById("vidasEnemigo").innerHTML-= 1
-		notificacion.innerHTML=e
-		}
-		else if(e=="PIERDES"){
-			document.getElementById("vidasJugador").innerHTML-=1
-			notificacion.innerHTML=e
-		}
-		else {
-			notificacion.innerHTML=e
-		}
-
-
-
-	
-
-	}
-
-	//-----------------------
-	if(ataquePC == ataqueJugador){
-		crearNotificacion("EMPATE")
-	}
-	else if((ataqueJugador== "AGUA") && (ataquePC== "FUEGO")){
-		
-		crearNotificacion("GANAS")
-
-	}
-
-	else if((ataqueJugador== "TIERRA") && (ataquePC== "AGUA")){
-		crearNotificacion("GANAS")
-
-	}
-	else if((ataqueJugador== "FUEGO") && (ataquePC== "TIERRA")){
-		crearNotificacion("GANAS")
-
-	}
-	else{
-		crearNotificacion("PIERDES")
-
-
-	}
-}
-
-
-
-
-
-
-
-
-
-
-let botonReinicio=document.getElementById("boton-reiniciar")
-	botonReinicio.addEventListener("click",()=>reset())
-
-function reset() {
-	location.reload()
-}
-
-//seccion juego ---------------------
+	if(ataquePC == ataqueJugador){crearNotificacion("EMPATE")}
+	else if((ataqueJugador== "AGUA") && (ataquePC== "FUEGO")){crearNotificacion("GANAS")}
+	else if((ataqueJugador== "TIERRA") && (ataquePC== "AGUA")){crearNotificacion("GANAS")}
+	else if((ataqueJugador== "FUEGO") && (ataquePC== "TIERRA")){crearNotificacion("GANAS")}
+	else{crearNotificacion("PIERDES")}}
+botonReinicio.addEventListener("click",()=>location.reload())
